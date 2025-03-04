ebf8c8fb051856012dc6659220cd820c0e30308c
nam-techie <namdpse180259@fpt.edu.vn>
2025-03-04 23:40:13 +0700

    done add audio

--- js/capoo.js
+++ js/capoo.js
@@ -1,5 +1,5 @@
 function nextPage() {
-    window.location.href = "imposter.html"; // Điều hướng đến trang Yes
+    window.location.href = "background-night-scenery.html"; // Điều hướng đến trang Yes
 }
 
 function moveButton() {
